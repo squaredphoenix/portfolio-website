b6484aee2b1d1da1e16a748ba5bd10d20888c1b3
squaredphoenix <nje9@students.uwf.edu>
2023-04-08 13:20:09 -0500

    updated website
    updated website

---
+++ Load.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $("#aboutMe").load("AboutMe.html");
+    $("#Certifications").load("Certifications.html");
+    $("#college").load("college.html");
+    $("#work").load("Work.html");
+    $("#project").load("Project.html");
+
+});
