90e51582315dfe33e09f4e37b6730a6b05eccc0c
Nicholas Eriser <nje9@students.uwf.edu>
2024-07-28 16:38:20 -0400

    cleaning up repository

--- Load.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $("#aboutMe").load("AboutMe.html");
-  $("#Skills").load("Skills.html");
-  $("#Certifications").load("Certifications.html");
-  $("#college").load("College.html");
-  $("#work").load("Work.html");
-  $("#project").load("Project.html");
-});
